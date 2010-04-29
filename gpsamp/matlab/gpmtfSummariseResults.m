bdeebb46ab6be72f68f4567579722157f06257d0
mtitsias <mtitsias@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2010-04-29 12:29:29 +0000

    Chib's marginal likelihood

--- gpsamp/matlab/gpmtfSummariseResults.m
+++ gpsamp/matlab/gpmtfSummariseResults.m
@@ -1,26 +1,154 @@
-function results = gpmtfSummariseResults(testGenes, method),
+function results = gpmtfSummariseResults(testGenes, method, Genes, GeneVars, TFs, models)
 
 if nargin < 2,
   method='zscore';
 end
 
 
-% Computation of the marginal likelihoods based on Jensen's inequality 
-% (This is also a more robust/average of the Chib's approximation for computing
-% marginal likelihoods) 
-if strcmp(method, 'margLogLik1')
-  %  
+if strcmp(method, 'margLogLik2')
+  % Chib's approximatino to the marginal likelihood  
   results = zeros( size(testGenes) );
   % compute all marginal likelihoods for all trained models
   for k=1:size(testGenes,1) 
   for c=1:size(testGenes,2) 
+  for f=1:size(testGenes,3)    
     %
     % approximate the entropy using Gaussian density estimation 
-    if isfield(testGenes{k,c}, 'W')
-       X = [testGenes{k,c}.kinetics; testGenes{k,c}.W; testGenes{k,c}.W0; testGenes{k,c}.sigma2]';
+    if isfield(testGenes{k,c,f}, 'W')
+       X = [testGenes{k,c,f}.kinetics; testGenes{k,c,f}.W; testGenes{k,c,f}.W0; testGenes{k,c,f}.sigma2]';
+       % Store also a representative paramrters for the Chib;s marginal
+       % likelihood (currently is the max in term of fititng the data)
+       [mm mind] = max(testGenes{k,c,f}.LogL);
+       parmu = [testGenes{k,c,f}.kinetics(:,mind); testGenes{k,c,f}.W(:,mind);...
+               testGenes{k,c,f}.W0(1,mind); testGenes{k,c,f}.sigma2(1,mind)]';
        %X = [testGenes{k,c}.kinetics; testGenes{k,c}.W; testGenes{k,c}.W0]';
     else
-       X = [testGenes{k,c}.kinetics; testGenes{k,c}.sigma2]'; 
+       X = [testGenes{k,c,f}.kinetics; testGenes{k,c,f}.sigma2]'; 
+       % Store also a representative paramrters for the Chib;s marginal
+       % likelihood (currently is the max in term of fititng the data)
+       [mm mind] = max(testGenes{k,c,f}.LogL);
+       parmu = [testGenes{k,c,f}.kinetics(:,mind); testGenes{k,c,f}.sigma2(1,mind)]';
+       %X = [testGenes{k,c}.kinetics]';
+    end
+    
+    mu = mean(X,1);
+    jit = 1e-8;
+    Sigma = cov(X) + jit*eye(size(X,2));
+    [N D] = size(X);                              
+    logPosteriorOfSelectedPoint = - (0.5*D)*log(2*pi)  - 0.5*log(det(Sigma)) ...
+                                  - 0.5*((parmu-mu)*(Sigma\(parmu-mu)'));
+    
+    % Chib's approximation to the marginal likelihood 
+    %
+    % use values with likelhiood under the model and evaluate the Chib's 
+    % method there 
+    if size(testGenes,3) > 1    
+        modelTest = models{c,f};
+    else
+        modelTest = models{c};
+    end
+    modelTest.Likelihood.Genes = Genes(k,:,:);
+    if isfield(modelTest.Likelihood.noiseModel, 'pumaSigma2') == 1
+        modelTest.Likelihood.noiseModel.pumaSigma2(1,:,:) = GeneVars(k,:,:);
+    end
+    if modelTest.Likelihood.numTFs == 0
+    %    
+       modelTest.Likelihood.kinetics(1) = parmu(1);
+       modelTest.Likelihood.kinetics(2) = parmu(2);
+       modelTest.Likelihood.kinetics(3) = parmu(3);
+       modelTest.Likelihood.noiseModel.sigma2 = parmu(4);
+       if ~isfield(modelTest.Likelihood, 'crValMask') 
+       modelTest.Likelihood.crValMask = 1:modelTest.Likelihood.numTimes;
+       end
+       % compute the maeginal likelhiod (GPs fucntino marginalized out)
+       PredGenes = parmu(1)/parmu(2)  + (parmu(3) - parmu(1)/parmu(2))*exp(-modelTest.Likelihood.TimesG*parmu(2));
+       L = 0;
+       %     
+       for r=1:modelTest.Likelihood.numReplicas
+            sigmas = zeros(1, modelTest.Likelihood.numTimes);
+            if modelTest.Likelihood.noiseModel.active(1) == 1
+                sigmas = modelTest.Likelihood.noiseModel.pumaSigma2(1, : , r);
+            end
+            if modelTest.Likelihood.noiseModel.active(2) == 1
+                sigmas = sigmas + repmat(modelTest.Likelihood.noiseModel.sigma2, 1, modelTest.Likelihood.numTimes ); 
+            end
+            L = L - 0.5*sum(log(2*pi*sigmas(modelTest.Likelihood.crValMask)),2)....
+                  - 0.5*sum(((modelTest.Likelihood.Genes(1, modelTest.Likelihood.crValMask, r)...
+                  - PredGenes(modelTest.Likelihood.crValMask)).^2)./sigmas(modelTest.Likelihood.crValMask),2);
+       end
+       LogLik = ones(1,size(TFs,2))*L;
+    else
+      modelTest.Likelihood.kinetics(1,:) = parmu(1:4);
+      modelTest.Likelihood.W(1,:) = parmu(5:4+modelTest.Likelihood.numTFs);
+      modelTest.Likelihood.W0 = parmu(5+modelTest.Likelihood.numTFs);
+      modelTest.Likelihood.noiseModel.sigma2 = parmu(end);
+      TFset = find(modelTest.Likelihood.TFcomb==1);
+      LogLik = zeros(1,size(TFs,2));
+      %     
+      for t=1:size(TFs,2)
+         modelTest.Likelihood.TF = TFs{t}(TFset,:,:);
+         for r=1:modelTest.Likelihood.numReplicas          
+            LogLik(t) = LogLik(t) + gpmtfLogLikelihoodGene(modelTest.Likelihood, zeros(size(modelTest.Likelihood.TF(:,:,1))), r, 1);
+         end   
+      end
+    %  
+    end
+    % evaluate the priors on the mu vectro
+    lnpriorKin = ['ln',modelTest.prior.kinetics.type,'pdf'];
+    TrspaceKin = modelTest.prior.kinetics.priorSpace; 
+    Likkin = feval(TrspaceKin, modelTest.Likelihood.kinetics);
+    LogPriorKin = feval(lnpriorKin, Likkin, modelTest.prior.kinetics);
+      
+    % white GP noise model is used
+    lnpriorNoiseWHITE = ['ln',modelTest.prior.sigma2.type,'pdf'];
+    TrspaceNoiseWHITE = modelTest.prior.sigma2.priorSpace; 
+    temp = feval(TrspaceNoiseWHITE, modelTest.Likelihood.noiseModel.sigma2);
+    LogPriorNoiseWHITE = feval(lnpriorNoiseWHITE, temp, modelTest.prior.sigma2);
+    
+    LogPrior = sum(LogPriorKin + LogPriorNoiseWHITE);
+    
+    if modelTest.Likelihood.numTFs > 0
+    % evaluation of the prior for the interaction bias
+    lnpriorW0 = ['ln',modelTest.prior.W0.type,'pdf'];
+    TrspaceW0 = modelTest.prior.W0.priorSpace;  
+    LikW0 = feval(TrspaceW0, modelTest.Likelihood.W0);
+    LogPriorW0 = feval(lnpriorW0, LikW0, modelTest.prior.W0);
+    % evaluation of the prior for the interaction weights
+    lnpriorW = ['ln',modelTest.prior.W.type,'pdf'];
+    TrspaceW = modelTest.prior.W.priorSpace; 
+    LikW = feval(TrspaceW, modelTest.Likelihood.W);
+    LogPriorW = feval(lnpriorW, LikW, modelTest.prior.W);
+    %
+    LogPrior = LogPrior + sum(LogPriorW0 + LogPriorW);
+    %
+    end
+    %mm
+    %logsumexp(LogLik)
+    %max(LogLik)
+    %modelTest.Likelihood.numTFs
+    %pause
+    LogMargL(k,c,f) = logsumexp(LogLik) - log(size(TFs,2))  +  LogPrior - logPosteriorOfSelectedPoint;
+    %
+  end
+  end
+  end
+  results = real(LogMargL);
+  return;
+  %
+elseif strcmp(method, 'margLogLik1')
+  % 
+  results = zeros( size(testGenes) );
+  % compute all marginal likelihoods for all trained models
+  for k=1:size(testGenes,1) 
+  for c=1:size(testGenes,2) 
+  for f=1:size(testGenes,3)    
+    %
+    % approximate the entropy using Gaussian density estimation 
+    if isfield(testGenes{k,c,f}, 'W')
+       X = [testGenes{k,c,f}.kinetics; testGenes{k,c,f}.W; testGenes{k,c,f}.W0; testGenes{k,c,f}.sigma2]';
+       %X = [testGenes{k,c}.kinetics; testGenes{k,c}.W; testGenes{k,c}.W0]';
+    else
+       X = [testGenes{k,c,f}.kinetics; testGenes{k,c,f}.sigma2]'; 
        %X = [testGenes{k,c}.kinetics]';
     end
     mu = mean(X,1);
@@ -28,8 +156,9 @@ if strcmp(method, 'margLogLik1')
     [N D] = size(X);
     jit = 1e-10;
     entropy = (0.5*D)*log(2*pi) + (0.5*D) + 0.5*log(det(Sigma + jit*eye(size(Sigma,1))));
-    LogMargL(k,c) = mean(testGenes{k,c}.LogL) + mean(testGenes{k,c}.LogPrior) + entropy;
+    LogMargL(k,c,f) = mean(testGenes{k,c,f}.LogL) + mean(testGenes{k,c,f}.LogPrior) + entropy;
     %
+  end
   end
   end
   results = real(LogMargL);
@@ -109,3 +238,4 @@ for k=1:length(testGenes),
     results(:, k) = max(mean(W < -.02, 2), mean(W > .02, 2));
   end
 end
+
